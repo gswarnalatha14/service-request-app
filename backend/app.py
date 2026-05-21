8b3bae477591450e8b76480ad3b762c24e989865
gswarnalatha14 <gswarna819@gmail.com>
2026-05-21 21:50:17 +0530

    Add MYSQL_PORT configuration to app settings

--- backend/app.py
+++ backend/app.py
@@ -14,6 +14,7 @@ app.config['MYSQL_HOST'] = config.MYSQL_HOST
 app.config['MYSQL_USER'] = config.MYSQL_USER
 app.config['MYSQL_PASSWORD'] = config.MYSQL_PASSWORD
 app.config['MYSQL_DB'] = config.MYSQL_DB
+app.config['MYSQL_PORT'] = config.MYSQL_PORT
 
 # Upload Folder
 app.config['UPLOAD_FOLDER'] = config.UPLOAD_FOLDER
